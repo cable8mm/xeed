6ab11aba4479b205eba099b327fd6ad949ef62ef
Sam Lee <cable8mm@gmail.com>
2024-03-15 10:20:23 +1300

    Generate `seeder`s command creation

---
+++ src/Command/GenerateSeedersCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Cable8mm\Xeed\Command;
+
+use Cable8mm\Xeed\DB;
+use Cable8mm\Xeed\Generators\SeederGenerator;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'generate-seeders',
+    description: 'Generate seeders. run `bin/console generate-seeders`',
+    hidden: false,
+    aliases: ['seeders']
+)]
+class GenerateSeedersCommand extends Command
+{
+    protected function configure()
+    {
+        $dotenv = \Dotenv\Dotenv::createImmutable(getcwd());
+        $dotenv->safeLoad();
+    }
+
+    /**
+     * Generate models.
+     *
+     * Run `bin/console generate-seeders` or `bin/console seeders`
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $tables = DB::getInstance()->attach()->getTables();
+
+        foreach ($tables as $table) {
+            SeederGenerator::make($table)->run();
+        }
+
+        return Command::SUCCESS;
+    }
+}
--- src/Generators/SeederGenerator.php
+++ src/Generators/SeederGenerator.php
@@ -3,7 +3,9 @@
 namespace Cable8mm\Xeed\Generators;
 
 use Cable8mm\Xeed\Interfaces\Generator;
+use Cable8mm\Xeed\Support\Inflector;
 use Cable8mm\Xeed\Support\Path;
+use Cable8mm\Xeed\Table;
 
 /**
  * Generator for `dist/database/seeders/*.php`.
@@ -58,15 +60,20 @@ final class SeederGenerator implements Generator
     /**
      * Factory method.
      *
-     * @param  string  $class  The model class name
+     * @param  string|Table  $class  The model class name
      * @param  string  $namespace  The model namespace
      * @param  string  $dist  The path to the dist folder
      */
     public static function make(
-        string $class,
+        string|Table $class,
         ?string $namespace = null,
         ?string $dist = null
     ): static {
+
+        if ($class instanceof Table) {
+            $class = Inflector::classify($class->name);
+        }
+
         return new self($class, $namespace, $dist);
     }
 }
