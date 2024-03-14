ca1fbda52a6212a6478d184e5e43ec4af85f3b35
Sam Lee <cable8mm@gmail.com>
2024-03-14 18:24:26 +1300

    Fix a bug to pass DBTest

--- tests/Unit/DBTest.php
+++ tests/Unit/DBTest.php
@@ -2,11 +2,29 @@
 
 namespace Cable8mm\Xeed\Tests\Unit;
 
+use Cable8mm\Xeed\Database\Seeder;
 use Cable8mm\Xeed\DB;
 use PHPUnit\Framework\TestCase;
 
 final class DBTest extends TestCase
 {
+    private Seeder $seeder;
+
+    protected function setUp(): void
+    {
+        $dotenv = \Dotenv\Dotenv::createImmutable(getcwd());
+        $dotenv->safeLoad();
+
+        $this->seeder = new Seeder();
+
+        $this->seeder->run();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->seeder->dropTables();
+    }
+
     public function test_db_instance_can_be_created(): void
     {
         $db = DB::newGetInstance();
