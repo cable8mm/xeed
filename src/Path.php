6d0fc42654342944d33c0cafc3da08de8e1cc0c3
Sam Lee <cable8mm@gmail.com>
2024-03-14 22:26:16 +1300

    Revise the folder structure for distribution

---
+++ src/Path.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Cable8mm\Xeed;
+
+final class Path
+{
+    public static function stub(): string
+    {
+        return getcwd().DIRECTORY_SEPARATOR.'stubs'.DIRECTORY_SEPARATOR;
+    }
+
+    public static function model(): string
+    {
+        return getcwd().DIRECTORY_SEPARATOR.'dist'.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'Models'.DIRECTORY_SEPARATOR;
+    }
+
+    public static function seeder(): string
+    {
+        return getcwd().DIRECTORY_SEPARATOR.'dist'.DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR.'seeders'.DIRECTORY_SEPARATOR;
+    }
+
+    public static function factory(): string
+    {
+        return getcwd().DIRECTORY_SEPARATOR.'dist'.DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR.'factories'.DIRECTORY_SEPARATOR;
+    }
+
+    public static function migration(): string
+    {
+        return getcwd().DIRECTORY_SEPARATOR.'dist'.DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR;
+    }
+}
--- tests/Unit/SeederGeneratorTest.php
+++ tests/Unit/SeederGeneratorTest.php
@@ -3,20 +3,18 @@
 namespace Cable8mm\Xeed\Tests\Unit;
 
 use Cable8mm\Xeed\Generators\SeederGenerator;
+use Cable8mm\Xeed\Path;
 use PHPUnit\Framework\TestCase;
 
 final class SeederGeneratorTest extends TestCase
 {
     public function test_it_can_can_generate_seeder(): void
     {
-        $seederGenerator = SeederGenerator::make(
-            'Sample',
-            namespace: '\App\Models'
-        );
+        $seederGenerator = SeederGenerator::make('Sample');
 
         $seederGenerator->run();
 
-        $outputPath = __DIR__.'/../../dist/SampleSeeder.php';
+        $outputPath = Path::seeder().'SampleSeeder.php';
 
         $this->assertFileExists($outputPath);
     }
