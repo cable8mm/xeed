947a15542f56b6aacbe8fe02122adfb60a6116bc
Sam Lee <cable8mm@gmail.com>
2024-03-21 22:52:41 +1300

    Create the `artisan xeed:clean` command

---
+++ tests/Laravel/Commands/CleanCommandTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Cable8mm\Xeed\Tests\Laravel\Commands;
+
+class CleanCommandTest extends \Orchestra\Testbench\TestCase
+{
+    public function test_execute_xeed_clean_command()
+    {
+        $this->artisan('xeed:clean')
+            ->expectsChoice(
+                'Please select directory for you to want to clean.',
+                'exit',
+                ['seeder', 'model', 'factory', 'migration', 'all', 'exit']
+            )
+            ->expectsOutput('You have just selected: exit')
+            ->assertExitCode(0);
+    }
+
+    public function test_execute_xeed_clean_all_command()
+    {
+        $this->artisan('xeed:clean')
+            ->expectsChoice(
+                'Please select directory for you to want to clean.',
+                'all',
+                ['seeder', 'model', 'factory', 'migration', 'all', 'exit']
+            )
+            ->expectsOutput('You have just selected: all')
+            ->assertExitCode(0);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            'Cable8mm\Xeed\Laravel\XeedServiceProvider',
+        ];
+    }
+}
