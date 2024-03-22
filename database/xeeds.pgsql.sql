d736f6a50f475bcb03e7568097a127c6dee5f689
Sam Lee <cable8mm@gmail.com>
2024-03-23 01:06:54 +1300

    Remove comments from pgsql.sql

--- database/xeeds.pgsql.sql
+++ database/xeeds.pgsql.sql
@@ -1,10 +1,4 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 14.11 (Homebrew)
--- Dumped by pg_dump version 14.11 (Homebrew)
-
+DROP TABLE IF EXISTS public.xeeds;
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -15,29 +9,8 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-
---
--- Name: postgis; Type: EXTENSION; Schema: -; Owner: -
---
-
--- CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
-
-
---
--- Name: EXTENSION postgis; Type: COMMENT; Schema: -; Owner: -
---
-
--- COMMENT ON EXTENSION postgis IS 'PostGIS geometry and geography spatial types and functions';
-
-
 SET default_tablespace = '';
-
 SET default_table_access_method = heap;
-
---
--- Name: xeeds; Type: TABLE; Schema: public; Owner: -
---
-
 CREATE TABLE public.xeeds (
     id bigint NOT NULL,
     big_integer bigint NOT NULL,
@@ -47,7 +20,7 @@ CREATE TABLE public.xeeds (
     date_time_tz timestamp(0) with time zone NOT NULL,
     date_time timestamp(0) without time zone NOT NULL,
     date date NOT NULL,
-    "decimal" numeric(8,2) NOT NULL,
+    "decimal" numeric(8, 2) NOT NULL,
     double double precision NOT NULL,
     enum character varying(255) NOT NULL,
     "float" real NOT NULL,
@@ -97,95 +70,35 @@ CREATE TABLE public.xeeds (
     ulid character(26) NOT NULL,
     uuid uuid NOT NULL,
     year integer NOT NULL,
-    CONSTRAINT xeeds_enum_check CHECK (((enum)::text = ANY ((ARRAY['easy'::character varying, 'hard'::character varying])::text[])))
+    CONSTRAINT xeeds_enum_check CHECK (
+        (
+            (enum)::text = ANY (
+                (
+                    ARRAY ['easy'::character varying, 'hard'::character varying]
+                )::text []
+            )
+        )
+    )
 );
-
-
---
--- Name: xeeds_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.xeeds_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: xeeds_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
+CREATE SEQUENCE public.xeeds_id_seq START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
 ALTER SEQUENCE public.xeeds_id_seq OWNED BY public.xeeds.id;
-
---
--- Name: xeeds id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.xeeds ALTER COLUMN id SET DEFAULT nextval('public.xeeds_id_seq'::regclass);
-
---
--- Name: xeeds xeeds_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
 ALTER TABLE ONLY public.xeeds
-    ADD CONSTRAINT xeeds_pkey PRIMARY KEY (id);
-
-
---
--- Name: xeeds_morphs_type_morphs_id_index; Type: INDEX; Schema: public; Owner: -
---
-
+ALTER COLUMN id
+SET DEFAULT nextval('public.xeeds_id_seq'::regclass);
+ALTER TABLE ONLY public.xeeds
+ADD CONSTRAINT xeeds_pkey PRIMARY KEY (id);
 CREATE INDEX xeeds_morphs_type_morphs_id_index ON public.xeeds USING btree (morphs_type, morphs_id);
-
-
---
--- Name: xeeds_nullable_morphs_type_nullable_morphs_id_index; Type: INDEX; Schema: public; Owner: -
---
-
 CREATE INDEX xeeds_nullable_morphs_type_nullable_morphs_id_index ON public.xeeds USING btree (nullable_morphs_type, nullable_morphs_id);
-
-
---
--- Name: xeeds_nullable_ulid_morphs_type_nullable_ulid_morphs_id_index; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX xeeds_nullable_ulid_morphs_type_nullable_ulid_morphs_id_index ON public.xeeds USING btree (nullable_ulid_morphs_type, nullable_ulid_morphs_id);
-
-
---
--- Name: xeeds_nullable_uuid_morphs_type_nullable_uuid_morphs_id_index; Type: INDEX; Schema: public; Owner: -
---
-
-CREATE INDEX xeeds_nullable_uuid_morphs_type_nullable_uuid_morphs_id_index ON public.xeeds USING btree (nullable_uuid_morphs_type, nullable_uuid_morphs_id);
-
-
---
--- Name: xeeds_ulid_morphs_type_ulid_morphs_id_index; Type: INDEX; Schema: public; Owner: -
---
-
+CREATE INDEX xeeds_nullable_ulid_morphs_type_nullable_ulid_morphs_id_index ON public.xeeds USING btree (
+    nullable_ulid_morphs_type,
+    nullable_ulid_morphs_id
+);
+CREATE INDEX xeeds_nullable_uuid_morphs_type_nullable_uuid_morphs_id_index ON public.xeeds USING btree (
+    nullable_uuid_morphs_type,
+    nullable_uuid_morphs_id
+);
 CREATE INDEX xeeds_ulid_morphs_type_ulid_morphs_id_index ON public.xeeds USING btree (ulid_morphs_type, ulid_morphs_id);
-
-
---
--- Name: xeeds_uuid_morphs_type_uuid_morphs_id_index; Type: INDEX; Schema: public; Owner: -
---
-
 CREATE INDEX xeeds_uuid_morphs_type_uuid_morphs_id_index ON public.xeeds USING btree (uuid_morphs_type, uuid_morphs_id);
-
-
---
--- PostgreSQL database dump complete
---
-
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 14.11 (Homebrew)
--- Dumped by pg_dump version 14.11 (Homebrew)
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -196,19 +109,4 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-
---
--- Data for Name: migrations; Type: TABLE DATA; Schema: public; Owner: -
---
-
---
--- Name: migrations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
---
-
 SELECT pg_catalog.setval('public.migrations_id_seq', 1, true);
-
-
---
--- PostgreSQL database dump complete
---
-
