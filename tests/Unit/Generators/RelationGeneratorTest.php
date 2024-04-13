83242ac5b6ab7a50fbfc2e187d6c8052aadd848a
Lopoi <42871409+Lopoi@users.noreply.github.com>
2024-04-12 23:52:36 -0300

    Change tests for phpunit 9.6
    using `assertFileEquals` instead of `assertFileMatchesFormat`

--- tests/Unit/Generators/RelationGeneratorTest.php
+++ tests/Unit/Generators/RelationGeneratorTest.php
@@ -58,46 +58,12 @@ final class RelationGeneratorTest extends TestCase
 
     public function test_it_can_generate_relations(): void
     {
-        $this->assertFileMatchesFormat(
-            '<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Sample extends Model
-{
-    use HasFactory;
-
-    public function related()
-    {
-        return $this->belongsTo(Related::class, \'related_id\');
-    }
-
-
-}',
+        $this->assertFileEquals(
+            Path::testExpected().DIRECTORY_SEPARATOR.'Sample.php',
             Path::testgen().DIRECTORY_SEPARATOR.'Sample.php'
         );
-        $this->assertFileMatchesFormat(
-            '<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Related extends Model
-{
-    use HasFactory;
-
-
-    public function samples()
-    {
-        return $this->hasMany(Sample::class, \'related_id\');
-    }
-
-}',
+        $this->assertFileEquals(
+            Path::testExpected().DIRECTORY_SEPARATOR.'Related.php',
             Path::testgen().DIRECTORY_SEPARATOR.'Related.php'
         );
     }
