7a2a5ea0440260cbbd2529fdca397b87e85659e2
Samgu Lee <cable8mm@gmail.com>
2025-01-06 18:10:52 +0900

    Fix remove useless fields from nova resources (#96)
    * Fix remove useless fields from nova resources
    
    * Refactoring a delete method

--- tests/Unit/Generators/NovaResourceGeneratorTest.php
+++ tests/Unit/Generators/NovaResourceGeneratorTest.php
@@ -3,17 +3,17 @@
 namespace Cable8mm\Xeed\Tests\Unit\Generators;
 
 use Cable8mm\Xeed\Column;
-use Cable8mm\Xeed\Generators\ModelGenerator;
+use Cable8mm\Xeed\Generators\NovaResourceGenerator;
 use Cable8mm\Xeed\Support\File;
 use Cable8mm\Xeed\Support\Path;
 use Cable8mm\Xeed\Table;
 use PHPUnit\Framework\TestCase;
 
-final class ModelGeneratorTest extends TestCase
+final class NovaResourceGeneratorTest extends TestCase
 {
     protected function setUp(): void
     {
-        ModelGenerator::make(
+        NovaResourceGenerator::make(
             new Table('samples', [
                 Column::make('id', 'bigint'),
                 Column::make('created_at', 'timestamp'),
@@ -28,31 +28,16 @@ final class ModelGeneratorTest extends TestCase
         File::system()->delete(Path::testgen().DIRECTORY_SEPARATOR.'Sample.php');
     }
 
-    public function test_it_can_generate_model_file(): void
+    public function test_it_can_be_existed(): void
     {
         $this->assertFileExists(Path::testgen().DIRECTORY_SEPARATOR.'Sample.php');
     }
 
-    public function test_it_can_generate_timestamps(): void
+    public function test_it_can_be_without_timestamps(): void
     {
         $file = File::system()->read(Path::testgen().DIRECTORY_SEPARATOR.'Sample.php');
 
-        $this->assertStringNotContainsString('timestamps', $file);
-    }
-
-    public function test_it_can_generate_without_timestamps(): void
-    {
-        File::system()->delete(Path::testgen().DIRECTORY_SEPARATOR.'Sample.php');
-
-        ModelGenerator::make(
-            new Table('samples', [
-                Column::make('id', 'bigint'),
-            ]),
-            destination: Path::testgen()
-        )->run();
-
-        $file = File::system()->read(Path::testgen().DIRECTORY_SEPARATOR.'Sample.php');
-
-        $this->assertStringContainsString('timestamps', $file);
+        $this->assertStringNotContainsString('Created At', $file);
+        $this->assertStringNotContainsString('Updated At', $file);
     }
 }
